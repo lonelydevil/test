69557624086f6aeaaa889639cd6276e9fd212fea
lonelydevil <harainanbaharahana@gmail.com>
2021-08-06 16:22:11 +0800

    project init

---
+++ Headers/std_c.h
@@ -0,0 +1,10 @@
+#ifndef STD_C_H
+#define STD_C_H
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "limits.h"
+#include "time.h"
+
+#endif // STD_C_H
---
+++ Sources/main.c
@@ -0,0 +1,5 @@
+#include "../Headers/std_c.h"
+
+int main(int argc, char **argv){
+	return 0;
+}
